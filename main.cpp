82e3071936368f3305d4a23c9d39a784024528ea
Du Park <dupark3@gmail.com>
2018-02-22 16:11:45 -0500

    method two using inserter instead of back_inserter

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <algorithm> // copy
 #include <iostream>
-#include <iterator> // back_inserter
+#include <iterator> // back_inserter, inserter
 #include <vector>
 
 int main() {
     std::vector<int> u(10, 100);
-    std::vector<int> v = u;
+    std::vector<int> v;
+    copy(u.begin(), u.end(), inserter(v, v.begin()));
 
+    // u contains 10 elements with value 100
     std::cout << "u: ";
     for (std::vector<int>::size_type i = 0; i != u.size(); ++i)
         std::cout << u[i] << " ";
     std::cout << std::endl;
 
+    // v also contains 10 elements with value 100
     std::cout << "v: ";
     for (std::vector<int>::size_type i = 0; i != v.size(); ++i)
         std::cout << v[i] << " ";
