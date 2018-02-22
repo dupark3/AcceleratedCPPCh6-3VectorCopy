bebaaa3ae9686b7dbc65e366845ed04d66b25ef4
Du Park <dupark3@gmail.com>
2018-02-22 16:06:31 -0500

    Simple assignment method

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
 
 int main() {
     std::vector<int> u(10, 100);
-    std::vector<int> v;
-
-    copy(u.begin(), u.end(), back_inserter(v));
+    std::vector<int> v = u;
 
     std::cout << "u: ";
     for (std::vector<int>::size_type i = 0; i != u.size(); ++i)
